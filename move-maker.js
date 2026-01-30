53b971b735ba64abdcd35867ccccaf46ae94de92
Trang <dothuytrang2312@gmail.com>
2026-01-30 21:28:44 +0100

    Delete 1 '=' in validateMove()'s conditions

--- move-maker.js
+++ move-maker.js
@@ -24,9 +24,10 @@ function validateMove(move, board) {
     if (moveStr.length === 3 && moveStr.includes(",")) {
         // 1st digit has to be from 1 to 3 only
         const moveArr = moveStr.split(",")
-        if (moveArr[0] === 1 || moveArr[0] === 2 || moveArr[0] === 3) {
+        if (moveArr[0] == 1 || moveArr[0] == 2 || moveArr[0] == 3) {
             // 2nd digit has to be from 1 to 3 only
-            if (moveArr[1] === 1 || moveArr[1] === 2 || moveArr[1] === 3)  {
+            console.log(moveArr)
+            if (moveArr[1] == 1 || moveArr[1] == 2 || moveArr[1] == 3)  {
                 // check if position is blank (_)
                 if (board[moveArr[0] - 1][moveArr[1] - 1] === '_') {
                     // --> valid -> return true
