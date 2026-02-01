02e69f2bf1400545ae545a263956236f13efdd52
marissamolejon <molejonmarissa@gmail.com>
2026-02-01 12:23:54 +0100

    final move

--- play.js
+++ play.js
@@ -1,11 +1,3 @@
-import prompt from 'prompt';
-import { isGameOver } from './status-checker.js';
-import { makeMove } from './move-maker.js';
-import { printBoard } from './board-printer.js';
-
-prompt.message = '';
-prompt.start();
-
 async function playGame() {
     let moveNumber = 0;
 
@@ -15,22 +7,19 @@ async function playGame() {
         ['_', '_', '_']
     ];
 
-    while(!isGameOver(board)) {
+    printBoard(board);
+
+    while (!isGameOver(board)) {
         let player = moveNumber % 2 === 0 ? 'X' : 'O';
         let move;
-        
+
         do {
             move = await getMove(player);
-        } while(!makeMove(board, move, player));
+        } while (!makeMove(board, move, player));
 
         printBoard(board);
         moveNumber++;
     }
-}
 
-async function getMove(player) {
-    let move = await prompt.get(player);
-    return move[player];
+    console.log("Game over!");
 }
-
-playGame();
